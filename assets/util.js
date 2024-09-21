025a7d5b03d224e1f058882e881b278c88f391b0
Bruno Mendes <bd_mendes@outlook.com>
2024-09-21 16:14:16 +0100

    Add clear search and cache poetry content on session

--- assets/util.js
+++ assets/util.js
@@ -9,3 +9,44 @@ function debounce(func, delay = 250) {
     }, delay);
   };
 }
+
+function hookPageCaching() {
+  // Restore the cached content
+  const cachedContent = sessionStorage.getItem("content");
+  if (cachedContent) {
+    document.getElementById("content").innerHTML = cachedContent;
+  }
+
+  // Restore form field values inside the #content div
+  const formFields = document.querySelectorAll(
+    "#content input, #content select, #content textarea"
+  );
+  formFields.forEach((field) => {
+    const cachedValue = sessionStorage.getItem(field.id);
+    if (cachedValue) {
+      if (field.type === "checkbox" || field.type === "radio") {
+        field.checked = cachedValue === "true";
+      } else {
+        field.value = cachedValue;
+      }
+    }
+  });
+
+  // Add event listener to cache the content and form field values before unload
+  window.addEventListener("beforeunload", () => {
+    // Cache the entire content's innerHTML
+    sessionStorage.setItem(
+      "content",
+      document.getElementById("content").innerHTML
+    );
+
+    // Cache form field values inside the #content div
+    formFields.forEach((field) => {
+      if (field.type === "checkbox" || field.type === "radio") {
+        sessionStorage.setItem(field.id, field.checked);
+      } else {
+        sessionStorage.setItem(field.id, field.value);
+      }
+    });
+  });
+}
