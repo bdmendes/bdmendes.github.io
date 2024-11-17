9f7b11d6c824dc06ed7f55c28dd470350b2180d0
Bruno Mendes <bd_mendes@outlook.com>
2024-11-17 01:03:55 +0000

    Improve description extractor

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,20 +1,17 @@
 import { GENERATE_SLUG_FROM_TITLE } from '../config'
 
-export function createSlug(title: string, staticSlug: string = title) {
-  return (
-    !GENERATE_SLUG_FROM_TITLE ? staticSlug : title
-      .trim()
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/\s+/g, '-')
-      .replace(/[^\w-]/g, '')
-      .replace(/^-+|-+$/g, '')
-  )
+export function createSlug(title: string) {
+  return title
+    .trim()
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]/g, '')
+    .replace(/^-+|-+$/g, '')
 }
 
 export function extractDate(path: string) {
-  // Regex to match the YYYY-MM-DD pattern
   const regex = /(\d{4}-\d{2}-\d{2})/;
   const match = path.match(regex) ?? ['1980-01-01'];
   return new Date(match[0])
@@ -23,28 +20,33 @@ export function extractDate(path: string) {
 export function extractDescription(body: string) {
   let processed = body
     .split('\n\n')[0]
-    .split('.')[0]
-    .replace(/\*/g, "")
-    .replace(/\>/g, "")
+    .replace(/[\*>]|\[\^[0-9]+\]/g, "")
     .replaceAll('\n', '')
     .replaceAll('  ', ' / ')
+    .replaceAll(/[,:.!?] \//g, ' /')
     .trim();
 
-  if (processed.endsWith('/')) {
-    processed = processed.substring(0, processed.length - 1);
-  }
-
-  for (let punct of [',', '?', '!', '.']) {
-    const max = punct == ',' ? 4 : 2;
-    if (processed.split(punct).length > max) {
-      processed = processed.split(punct).slice(0, max).join(punct);
+  const punct = [',', '?', '!', '.', ':', '/']
+  const max_size = 120
+  const max_size_small = 80
+  const max_puncts = 4
+  let seen_puncts = 0;
+  for (let i = 0; i < processed.length; i++) {
+    if (punct.includes(processed[i])) {
+      seen_puncts += 1;
+      if (i >= max_size || (processed[i] == '.' && i >= max_size_small) || seen_puncts >= max_puncts) {
+        processed = processed.substring(0, i).trimEnd() + '.';
+        break;
+      }
     }
   }
 
   processed = processed.trimEnd();
 
-  if (processed.endsWith('?') || processed.endsWith('!')) {
+  if (processed.endsWith('.')) {
     return processed;
+  } else if (punct.includes(processed[processed.length - 1])) {
+    return processed.substring(0, processed.length - 1).trimEnd() + '.';
   } else {
     return processed + '.';
   }
