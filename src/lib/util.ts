93b2a7c6e6d75c39ca00d02bfd28d53c9b272819
Bruno Mendes <bd_mendes@outlook.com>
2024-12-24 14:29:02 +0000

    Tweak responsiveness

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -24,7 +24,7 @@ export function augmentTagsWithDate(tags: string[] | undefined, path: string) {
 }
 
 export function extractDescription(body: string) {
-  let processed = (body
+  let processed = body
     .split('\n\n')
     .filter(s => s.trimStart()[0] !== ">" && s.trimStart()[0] !== "#") // Exclude quotes and headers
     .map(s => s.replace(/[\*>]|\[\^[0-9]+\]/g, "")) // Clean markdown elements
@@ -32,15 +32,23 @@ export function extractDescription(body: string) {
     .replace(/\n/g, '') // Remove newlines
     .replace(/ {2,}/g, ' / ') // Replace multiple spaces with slashes
     .replace(/[,:.!?] \//g, ' /') // Tidy up spacing around slashes
-    .trim());
+    .trim();
 
-  const punct = [',', '?', '!', '.', ':', '/']
-  const min_length = 140
+  const end_punct = ['?', '!', '.', '/']
+  const punct = [',', ':', ')'].concat(end_punct)
+  const min_length = 150
   const min_length_small = 100
 
+  let par_count = 0
   for (let i = 0; i < processed.length; i++) {
-    if (punct.includes(processed[i])) {
-      if (i >= min_length || (processed[i] == '.' && i >= min_length_small)) {
+    if (processed[i] == '(') {
+      par_count += 1;
+    } else if (processed[i] == ')') {
+      par_count -= 1;
+    }
+
+    if (par_count == 0 && punct.includes(processed[i])) {
+      if (i >= min_length || (end_punct.includes(processed[i]) && i >= min_length_small)) {
         processed = !['?', '!'].includes(processed[i])
           ? processed.substring(0, i).trimEnd() + '.'
           : processed.substring(0, i + 1);
