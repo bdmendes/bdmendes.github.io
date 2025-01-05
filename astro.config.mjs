5ca8f38a7cab4cc0279f896952ff1a68b1d19cda
Bruno Mendes <bd_mendes@outlook.com>
2025-01-05 12:40:02 +0000

    Update to astro 5.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bdmendes.com',
-  integrations: [icon(), sitemap(), tailwind({
-    applyBaseStyles: false,
-  })],
+  site: "https://bdmendes.com",
+  integrations: [
+    icon(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
