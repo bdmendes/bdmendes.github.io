5be8c78612f918ce508c42c5003fa52b4239601a
Bruno Mendes <bd_mendes@outlook.com>
2025-04-20 22:49:48 +0100

    Remove static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,6 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://bdmendes.com",
-  output: "static",
-  build: {
-    format: "file",
-  },
   integrations: [
     icon(),
     sitemap(),
